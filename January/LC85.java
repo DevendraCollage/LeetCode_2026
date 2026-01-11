f141d2e73481f8de911a8e1ba099c4fdbacdc23f
Devendra Parmar <23030501043@darshan.ac.in>
2026-01-11 17:52:11 +0530

    Day-5, Day-6 and Day-8 LC865, LC712 and LC85 solved

---
+++ January/LC85.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int maximalRectangle(char[][] matrix) {
+        // If there are no rows then return
+        if (matrix.length == 0) {
+            return 0;
+        }
+        // find the number of columns in a row.
+        int height[] = new int[matrix[0].length];
+        int largest = 0;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                int val = (matrix[i][j] - '0');
+                if (val == 0) {
+                    height[j] = 0;
+                } else {
+                    height[j] += val;
+                }
+            }
+
+            int maxArea = largestRectangleArea(height);
+            if (largest < maxArea) {
+                largest = maxArea;
+            }
+        }
+
+        return largest;
+    }
+
+    public int largestRectangleArea(int[] heights) {
+        int[] stack = new int[heights.length];
+        int index = -1;
+        int max = 0;
+        int n = heights.length;
+
+        for (int i = 0; i < n; i++) {
+            while (index != -1 && heights[stack[index]] > heights[i]) {
+                int h = heights[stack[index--]];
+                int ps = (index == -1) ? -1 : stack[index];
+                int w = i - ps - 1;
+                max = Math.max(max, h * w);
+            }
+            stack[++index] = i;
+        }
+
+        while (index != -1) {
+            int h = heights[stack[index--]];
+            int ps = (index == -1) ? -1 : stack[index];
+            int w = n - ps - 1;
+            max = Math.max(max, h * w);
+        }
+
+        return max;
+    }
+}
