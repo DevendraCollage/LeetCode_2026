e320188854387dc9f6f3c1bc4e96da37711324b1
Devendra Parmar <23030501043@darshan.ac.in>
2026-02-26 22:05:57 +0530

    Day-53 LC1404 solved

---
+++ February/LC1404.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int numSteps(String s) {
+        int ans = 0, carry = 0;
+        char arr[] = s.toCharArray();
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            if (arr[i] - '0' + carry == 1) {
+                carry = 1;
+                ans += 2;
+            } else {
+                ans++;
+            }
+        }
+
+        return ans + carry;
+    }
+}
