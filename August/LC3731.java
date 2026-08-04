f3447f0afd096ad651ebf82aa0dad24791473191
Devendra Parmar <23030501043@darshan.ac.in>
2026-08-05 00:01:30 +0530

    Day-181 LC3731 solved

---
+++ August/LC3731.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        Arrays.sort(nums);
+
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = nums[i - 1] + 1; j < nums[i]; j++) {
+                ans.add(j);
+            }
+        }
+
+        return ans;
+    }
+}
